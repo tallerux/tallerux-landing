84f25fb3ded96bd3969ab71f8ee900f4ace24bb4
damian <damian@zauberlabs.com>
2014-03-07 22:29:31 -0300

    adding node+grunt (it doesn't work)

---
+++ Gruntfile.js
@@ -0,0 +1,80 @@
+/*
+ * grunt-cli
+ * http://gruntjs.com/
+ *
+ * Copyright (c) 2012 Tyler Kellen, contributors
+ * Licensed under the MIT license.
+ * https://github.com/gruntjs/grunt-init/blob/master/LICENSE-MIT
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+ 
+  grunt.registerTask('watch', [ 'watch' ]);
+ 
+  grunt.initConfig({
+    concat: {
+      js: {
+        options: {
+          separator: ';'
+        },
+        src: [
+          'javascript/*.js'
+        ],
+        dest: 'public/js/main.min.js'
+      },
+    },
+    uglify: {
+      options: {
+        mangle: false
+      },
+      js: {
+        files: {
+          'public/js/main.min.js': ['public/js/main.min.js']
+        }
+      }
+    },
+    sass: {
+      dev: {
+        options: {
+          style: 'expanded',
+          compass: true
+        },
+        files: {
+          '/css/main.css': '<%= project.css %>'
+        }
+      },
+      dist: {
+        options: {
+          style: 'compressed',
+          compass: true
+        },
+        files: {
+          '/css/main.css': '<%= project.css %>'
+        }
+      }
+    },
+    watch: {
+      js: {
+        files: ['javascript/*.js'],
+        tasks: ['concat:js', 'uglify:js'],
+        options: {
+          livereload: true,
+        }
+      },
+      css: {
+        files: ['scss/*.scss'],
+        tasks: ['sass:dev'],
+        options: {
+          livereload: true,
+        }
+      }
+    }
+  });
+ 
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+};
