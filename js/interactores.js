c2de319711c9725a4449841c564b73f1ec64194a
nfrette <nfrette@gmail.com>
2014-06-18 23:33:27 -0300

    add viewport. fix html and css with @media. fix .js menu

--- js/interactores.js
+++ js/interactores.js
@@ -1,5 +1,22 @@
 $( document ).ready(function() {
-	$(document).scroll(function() {
-       $('#menu').toggle($(this).scrollTop() > 200);
-      });
+/*	$(document).scroll(function(event){
+		if ($(this).scrollTop() > 350)
+       			$('#menu').slideToggle();
+      });*/
+	$(document).scroll(function(){
+        if ($(this).scrollTop() > 350) {
+            $('#menu').slideDown(200);}
+
+        else {
+            $('#menu').slideUp(200);
+        }
+
+/*        if ($(this).scrollTop() < 2000) {
+            $('#menu').slideDown(200);}
+
+        else {
+            $('#menu').slideUp(200);
+        }*/
+
+	});
 });
