492640f6ed03cecc58f9daeacf649edda3db383c
nfrette <nfrette@gmail.com>
2014-07-26 06:07:59 -0300

    fix syles (video and colors) . work in jq, add functions.

--- interactores/js/interactores.js
+++ interactores/js/interactores.js
@@ -1,9 +1,20 @@
+/*Created for @nfrette */
+
 $( document ).ready(function() {
-/*	$(document).scroll(function(event){
-		if ($(this).scrollTop() > 350)
-       			$('#menu').slideToggle();
-      });*/
-	$(document).scroll(function(){
+	$(document).scroll(function(event){
+    var boton = $('.partners').offset().top;
+    var boton2 = $('footer').offset().top + 150;
+
+		if ($(this).scrollTop() >= boton && $(document).scrollTop() < boton2) {
+       			$('#menu .button').hide();
+            $('#menu').css("text-align","center");}
+          else {
+            $('#menu .button').show();
+            $('#menu').css("text-align","left");
+          }
+      });
+	
+  $(document).scroll(function(){
         if ($(this).scrollTop() > 350) {
             $('#menu').slideDown(200);}
 
